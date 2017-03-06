d0843e9a09b912f349af35a3132accb154afd731
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-03-06 13:47:10 -0800

    Reimplement main script
    - Add `run` function/task
    - Use `Task`'s arg facilities to generate the arg parser for `run`
      instead of creating it manually; this is easier and also ensures
      consistency
    - Use `run`'s arg parser in the main script
    - Remove parsing of options passed via `-o`; this is now handled
      automatically by `Task` (since the default value of run's `options`
      arg is `{}`)
    - Make some other related simplifications
    
    It might be possible to go a bit further down this road by using a
    `TaskRunner` instance to parse the raw `argv` list into the args for the
    main script and the remaining task args.

--- taskrunner/__main__.py
+++ taskrunner/__main__.py
@@ -1,20 +1,88 @@
-import argparse
-import json
 import os
 import sys
-import textwrap
 
 from . import __version__
+from .config import RawConfig
 from .runner import TaskRunner, TaskRunnerError
+from .task import task
 from .util import printer
 
 
 def main(argv=None):
+    argv = sys.argv[1:] if argv is None else argv
+    run_args = []
+    run_task = task(run)
+
+    optionals = run_task.optionals.values()
+    options_with_values = set()
+    for param in optionals:
+        if not param.is_bool:
+            options_with_values.update(run_task.arg_names_for_param(param))
+
+    option_value_expected = False
+
+    for i, s in enumerate(argv):
+        if s == '--':
+            task_args = argv[i:]
+            break
+        elif s.startswith('-'):
+            run_args.append(s)
+            if s in options_with_values:
+                option_value_expected = '=' not in s
+        elif option_value_expected:
+            run_args.append(s)
+            option_value_expected = False
+        else:
+            task_args = argv[i:]
+            break
+    else:
+        # No args or all args consumed
+        task_args = []
+
+    config = RawConfig(debug=False)
+    args = run_task.parse_args(config, run_args)
+
+    config_file = args.get('config_file')
+    if config_file is None:
+        if os.path.exists('tasks.cfg'):
+            args['config_file'] = 'tasks.cfg'
+
+    options = args.get('options', {})
+    for name, value in options.items():
+        if name in run_task.optionals:
+            printer.error(
+                'Cannot pass {name} via -o; use --{option_name} instead'
+                .format(name=name, option_name=name.replace('_', '-')))
+            return 1
+
+    if args.get('list_tasks'):
+        args['list_tasks'] = 'short' if '-l' in run_args else 'long'
+
+    try:
+        run((argv, run_args, task_args), **args)
+    except TaskRunnerError as exc:
+        printer.error(exc, file=sys.stderr)
+        return 1
+
+    return 0
+
+
+def run(args,
+        config_file=None,
+        env=None,
+        version=None,
+        options={},
+        tasks_module='tasks.py',
+        list_tasks=False,
+        echo=False,
+        hide=None,
+        info=False,
+        debug=False):
     """Run one or more tasks in succession.
 
     For example, assume the tasks ``local`` and ``remote`` have been
     defined; the following will run ``ls`` first on the local host and
-    then on the remote hos::
+    then on the remote host::
 
         runtasks local ls remote --host <host> ls
 
@@ -28,131 +96,46 @@ def main(argv=None):
     a value and not a task name.
 
     """
-    argv = sys.argv[1:] if argv is None else argv
-    command_args, remaining_args = split_args(argv)
+    argv, run_args, task_args = args
 
-    parser = argparse.ArgumentParser(
-        description=textwrap.dedent(''.join(('    ', main.__doc__.strip()))),
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-    )
+    print_and_exit = any((list_tasks, info, not argv))
 
-    parser.add_argument('-c', '--config-file', type=config_file_type, default='tasks.cfg')
-    parser.add_argument('-e', '--env', type=config_file_type, default=None)
-    parser.add_argument('-v', '--version', default=None)
-    parser.add_argument('-o', dest='options', action='append', default=[])
-    parser.add_argument('-t', '--tasks-module', default='tasks.py')
-    parser.add_argument('-l', dest='list_tasks_short', action='store_true', default=False)
-    parser.add_argument('--list', dest='list_tasks', action='store_true', default=False)
-    parser.add_argument('-E', '--echo', action='store_true', default=False)
-    parser.add_argument('--no-echo', action='store_false', dest='echo', default=False)
-    parser.add_argument('-H', '--hide', choices=('none', 'stdout', 'stderr', 'all'), default=None)
-    parser.add_argument('-d', '--debug', action='store_true', default=False)
-    parser.add_argument('-i', '--info', action='store_true', default=False)
-    args = parser.parse_args(command_args)
-
-    print_and_exit = any((
-        args.list_tasks,
-        args.list_tasks_short,
-        args.info,
-        not remaining_args,
-    ))
-
-    if print_and_exit or args.debug:
+    if print_and_exit or debug:
         print('TaskRunner version:', __version__)
 
-    if args.debug:
+    if debug:
         printer.debug('All args:', argv)
-        printer.debug('Runtasks args:', command_args)
-        printer.debug('All task args:', remaining_args)
-        args.echo = True
+        printer.debug('Run args:', run_args)
+        printer.debug('Task args:', task_args)
+        echo = True
 
-    if args.options:
-        options = {}
-        non_options = (
-            'config_file', 'env', 'version', 'tasks_module', 'echo', 'no_echo', 'hide', 'debug')
-        for item in args.options:
-            n, v = item.split('=', 1)
-            if n in non_options:
-                printer.error(
-                    'Cannot pass {name} via -o; use --{option_name} instead'
-                    .format(name=n, option_name=n.replace('_', '-')))
-                return 1
-            try:
-                v = json.loads(v)
-            except ValueError:
-                pass
-            options[n] = v
-    else:
-        options = {}
+    options = options or {}
 
-    if args.version is not None:
-        options['version'] = args.version
+    if version is not None:
+        options['version'] = version
 
     runner = TaskRunner(
-        config_file=args.config_file,
-        env=args.env,
+        config_file=config_file,
+        env=env,
         options=options,
-        tasks_module=args.tasks_module,
-        default_echo=args.echo,
-        default_hide=args.hide,
-        debug=args.debug,
+        tasks_module=tasks_module,
+        default_echo=echo,
+        default_hide=hide,
+        debug=debug,
     )
 
     if print_and_exit:
-        if args.list_tasks_short:
-            runner.print_usage(args.tasks_module, short=True)
-        elif args.list_tasks:
-            print()
-            runner.print_usage(args.tasks_module)
-        elif not remaining_args and not args.info:
+        if list_tasks:
+            if list_tasks in ('short', True):
+                runner.print_usage(tasks_module, short=True)
+            elif list_tasks == 'long':
+                print()
+                runner.print_usage(tasks_module)
+        elif not argv and not info:
             printer.warning('No tasks specified')
-            runner.print_usage(args.tasks_module, short=True)
+            runner.print_usage(tasks_module, short=True)
     else:
-        try:
-            runner.run(remaining_args)
-        except TaskRunnerError as exc:
-            printer.error(exc, file=sys.stderr)
-            return 1
-
-    return 0
-
-
-def split_args(argv):
-    command_args = []
-
-    options_with_values = {
-        '-c', '--config-file',
-        '-e', '--env',
-        '-v', '--version',
-        '-o',
-        '-t', '--tasks-module',
-        '--hide',
-    }
-    option_value_expected = False
-
-    for i, s in enumerate(argv):
-        if s.startswith('-'):
-            command_args.append(s)
-            if s in options_with_values:
-                option_value_expected = '=' not in s
-        elif option_value_expected:
-            command_args.append(s)
-            option_value_expected = False
-        else:
-            remaining_args = argv[i:]
-            break
-    else:
-        # No args or all args consumed
-        remaining_args = []
-
-    return command_args, remaining_args
-
-
-def config_file_type(value):
-    if value == 'tasks.cfg':
-        if not os.path.isfile('tasks.cfg'):
-            value = None
-    return value
+        runner.run(task_args)
 
 
 if __name__ == '__main__':
