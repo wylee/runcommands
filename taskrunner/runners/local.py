41d3bef5f90f1bf67f2057d6f1bcd709e46fc85f
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-03-15 16:54:25 -0700

    Move NonBlockingStreamReader to its own module for potential reuse

--- taskrunner/runners/local.py
+++ taskrunner/runners/local.py
@@ -1,14 +1,13 @@
-import locale
 import os
 import shlex
 import sys
 from subprocess import PIPE, Popen, TimeoutExpired
-from threading import Thread
 
 from ..util import Hide, printer
 from .base import Runner
 from .exc import RunAborted, RunError
 from .result import Result
+from .streams import NonBlockingStreamReader
 
 
 class LocalRunner(Runner):
@@ -88,32 +87,3 @@ class LocalRunner(Runner):
             raise RunError(return_code, out_str, err_str)
 
         return Result(return_code, out_str, err_str)
-
-
-class NonBlockingStreamReader(Thread):
-
-    def __init__(self, name, stream, buffer, hide, file, encoding=None):
-        name = '{name}-reader'.format(name=name)
-        super().__init__(name=name, daemon=True)
-        self.stream = stream
-        self.buffer = buffer
-        self.hide = hide
-        self.file = file
-        self.encoding = encoding or locale.getpreferredencoding(do_setlocale=False)
-        self.start()
-
-    def run(self):
-        while not self.stream.closed:
-            try:
-                bytes_ = self.stream.readline()
-            except ValueError:
-                break
-            if bytes_:
-                text = bytes_.decode(self.encoding)
-                self.buffer.append(text)
-                if not self.hide:
-                    self.file.write(text)
-                    self.file.flush()
-
-    def get_string(self):
-        return ''.join(self.buffer)
---
+++ taskrunner/runners/streams.py
@@ -0,0 +1,31 @@
+from locale import getpreferredencoding
+from threading import Thread
+
+
+class NonBlockingStreamReader(Thread):
+
+    def __init__(self, name, stream, buffer, hide, file, encoding=None):
+        name = '{name}-reader'.format(name=name)
+        super().__init__(name=name, daemon=True)
+        self.stream = stream
+        self.buffer = buffer
+        self.hide = hide
+        self.file = file
+        self.encoding = encoding or locale.getpreferredencoding(do_setlocale=False)
+        self.start()
+
+    def run(self):
+        while not self.stream.closed:
+            try:
+                bytes_ = self.stream.readline()
+            except ValueError:
+                break
+            if bytes_:
+                text = bytes_.decode(self.encoding)
+                self.buffer.append(text)
+                if not self.hide:
+                    self.file.write(text)
+                    self.file.flush()
+
+    def get_string(self):
+        return ''.join(self.buffer)
