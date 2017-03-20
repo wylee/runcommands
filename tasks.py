e4ca0287bf21664280ff0834ac29432d15a2406d
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-03-20 11:06:53 -0700

    Fix typos in lint task

--- tasks.py
+++ tasks.py
@@ -23,8 +23,8 @@ def lint(config):
     result = local(config, 'flake8 taskrunner', abort_on_failure=False)
     pieces_of_lint = len(result.stdout_lines)
     if pieces_of_lint:
-        s = '' if pieces_of_lint == 1 else ''
-        printer.error('{pieces_of_lint} piece{s} lint found'.format_map(locals()))
+        s = '' if pieces_of_lint == 1 else 's'
+        printer.error('{pieces_of_lint} piece{s} of lint found'.format_map(locals()))
     else:
         printer.success('No lint found')
 
