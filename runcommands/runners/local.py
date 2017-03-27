495cce4cbf3dbe6f7196a875b617ba18031ea840
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-03-27 15:20:11 -0700

    Add PTY support to local/LocalRunner
    The default is to use a PTY if the platform supports it. `--no-use-pty`
    can be passed to `local` to disable this default.
    
    TODO: Add `--use-pty` flag to main script.

--- runcommands/runners/local.py
+++ runcommands/runners/local.py
@@ -1,4 +1,5 @@
 import os
+import pty
 import shlex
 import sys
 from subprocess import PIPE, Popen, TimeoutExpired
@@ -15,7 +16,7 @@ class LocalRunner(Runner):
     """Run a command on the local host."""
 
     def run(self, cmd, cd=None, path=None, prepend_path=None, append_path=None, echo=False,
-            hide=None, timeout=None, debug=False):
+            hide=None, timeout=None, use_pty=True, debug=False):
         if isinstance(cmd, str):
             cmd_str = cmd
             exe = shlex.split(cmd)[0]
@@ -31,8 +32,12 @@ class LocalRunner(Runner):
         hide_stderr = Hide.hide_stderr(hide)
         echo = echo and not hide_stdout
 
+        if use_pty:
+            use_pty = pty and sys.stdout.isatty()
+            if not use_pty:
+                printer.warning('PTY not available')
+
         env = os.environ.copy()
-        env['PYTHONUNBUFFERED'] = '1'
 
         munge_path = path or prepend_path or append_path
 
@@ -55,11 +60,33 @@ class LocalRunner(Runner):
             printer.hr(color='echo')
 
         try:
-            with Popen(cmd, bufsize=0, cwd=cwd, env=env, shell=shell, stdout=PIPE, stderr=PIPE) as proc:
+            stdin = None
+
+            if use_pty:
+                out_pty_fd, stdout = pty.openpty()
+                err_pty_fd, stderr = pty.openpty()
+            else:
+                stdout = PIPE
+                stderr = PIPE
+
+            streams = {
+                'stdin': stdin,
+                'stdout': stdout,
+                'stderr': stderr,
+            }
+
+            with Popen(cmd, bufsize=0, cwd=cwd, env=env, shell=shell, **streams) as proc:
+                if use_pty:
+                    os.close(stdout)
+                    os.close(stderr)
+
+                out_stream = os.fdopen(out_pty_fd, 'rb', 0) if use_pty else proc.stdout
+                err_stream = os.fdopen(err_pty_fd, 'rb', 0) if use_pty else proc.stderr
+
                 try:
-                    out = NonBlockingStreamReader('out', proc.stdout, hide_stdout, sys.stdout)
-                    err = NonBlockingStreamReader('err', proc.stderr, hide_stderr, sys.stderr)
-                    return_code = proc.wait(timeout)
+                    out = NonBlockingStreamReader('out', out_stream, hide_stdout, sys.stdout)
+                    err = NonBlockingStreamReader('err', err_stream, hide_stderr, sys.stderr)
+                    return_code = proc.wait(timeout=timeout)
                     out.finish()
                     err.finish()
                 except:
