f9421166a464919e85d7c1486c7fefaf3aec0988
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-03-27 18:23:01 -0700

    In util.asset_path(), raise a more-useful error on ImportError

--- runcommands/util.py
+++ runcommands/util.py
@@ -97,16 +97,26 @@ def asset_path(path, format_kwargs={}):
     """
     if format_kwargs:
         path = path.format(**format_kwargs)
+
     if ':' in path:
         package_name, *rel_path = path.split(':', 1)
     else:
         package_name, rel_path = path, ()
-    package = importlib.import_module(package_name)
+
+    try:
+        package = importlib.import_module(package_name)
+    except ImportError:
+        raise ValueError(
+            'Could not get asset path for {path}; could not import package: {package_name}'
+            .format_map(locals()))
+
     if not hasattr(package, '__file__'):
         raise ValueError("Can't compute path relative to namespace package")
+
     package_path = os.path.dirname(package.__file__)
     path = os.path.join(package_path, *rel_path)
     path = os.path.normpath(os.path.abspath(path))
+
     return path
 
 
