9cb8fb84b650efef301d7ca0602848c0801ca28a
Wyatt Baldwin <self@wyattbaldwin.com>
2017-04-08 20:29:03 -0700

    Flesh out configure() some
    Allow more environment variables to be set.
    
    TODO: *Use* these environment variables.

--- runcommands/__init__.py
+++ runcommands/__init__.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from .command import DEFAULT_ENV, bool_or, command
@@ -7,6 +8,23 @@ __all__ = ['__version__', 'DEFAULT_ENV', 'bool_or', 'command']
 __version__ = '1.0a15.dev0'
 
 
-def configure(default_env=None):
-    if default_env is not None:
-        os.environ['RUNCOMMANDS_DEFAULT_ENV'] = 'dev'
+def configure(commands_module=None, config_file=None, default_env=None, env=None, echo=None,
+              hide=None, debug=None):
+    """Set environment variables.
+    
+    String values will be left as is; other types of values will be JSON
+    encoded.
+    
+    """
+    def encode(v):
+        if isinstance(v, str):
+            return v
+        return json.dumps(v)
+
+    os.environ['RUNCOMMANDS_COMMANDS_MODULE'] = encode(commands_module)
+    os.environ['RUNCOMMANDS_CONFIG_FILE'] = encode(config_file)
+    os.environ['RUNCOMMANDS_DEFAULT_ENV'] = encode(default_env)
+    os.environ['RUNCOMMANDS_ENV'] = encode(env)
+    os.environ['RUNCOMMANDS_ECHO'] = encode(echo)
+    os.environ['RUNCOMMANDS_HIDE'] = encode(hide)
+    os.environ['RUNCOMMANDS_DEBUG'] = encode(debug)
