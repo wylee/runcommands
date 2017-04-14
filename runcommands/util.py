14594a31cf214fe32c06e35434b5df25030cd367
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-04-14 14:40:04 -0700

    [docs] Add docstring to util.confirm()

--- runcommands/util.py
+++ runcommands/util.py
@@ -178,6 +178,30 @@ def as_tuple(items, sep=','):
 
 def confirm(config, prompt='Really?', color='warning', yes_values=('y', 'yes'),
             abort_on_unconfirmed=False, abort_options=None):
+    """Prompt for confirmation.
+
+    Confirmation can be aborted by typing in a no value instead of one
+    of the yes values or with Ctrl-C.
+
+    Args:
+        config (Mapping): Only used to format ``prompt``
+        prompt (str): Prompt to present user ["Really?"]
+        color (string|Color|bool) Color to print prompt string; can be
+            ``False`` or ``None`` to print without color ["yellow"]
+        yes_values (list[str]): Values user must type in to confirm
+            [("y", "yes")]
+        abort_on_unconfirmed (bool|int|str): When user does not confirm:
+            - If this is ``True``, print "Aborted" to stdout and exit
+              with code 0
+            - If this is an integer, print "Aborted" to stdout if 0 or
+              stderr otherwise and exit with this error code
+            - If this is a string, print it to stdout and exit with
+              code 0
+        abort_options (dict): Options to pass to :func:`abort` when not
+            confirmed (these options will override any options set via
+            ``abort_on_unconfirmed``)
+
+    """
     prompt = prompt.format(**config)
     prompt = '{prompt} [{yes_value}/N] '.format(prompt=prompt, yes_value=yes_values[0])
 
