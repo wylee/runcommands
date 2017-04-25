921bdcc5d6f6ac4be7dfd0015e5b5fd6d06e6486
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-04-25 11:44:12 -0700

    Raise exception when --debug is specified to main script
    I.e., instead of printing the exception and then exiting.

--- runcommands/__main__.py
+++ runcommands/__main__.py
@@ -7,15 +7,21 @@ from .util import printer
 
 
 def main(argv=None):
+    debug = None
     try:
         all_argv, run_argv, command_argv = partition_argv(argv)
         cli_args = run.parse_args(RawConfig(run=RunConfig()), run_argv)
         run_args = read_run_args(run)
         run_args.update(cli_args)
+        debug = run_args.get('debug', run.parameters['debug'].default)
         run.implementation(
             None, all_argv=all_argv, run_argv=run_argv, command_argv=command_argv,
             cli_args=cli_args, **run_args)
     except RunCommandsError as exc:
+        if debug or debug is None:
+            # User specified --debug OR processing didn't get far enough
+            # to determine whether user specified --debug.
+            raise
         printer.error(exc, file=sys.stderr)
         return 1
     return 0
