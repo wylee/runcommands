wylee/runcommands

commit 3d4327547245bda9cf187da3063998dd455ed922

Improve interpolation
The big change is that interpolation now works with/on any value read
from a config file.

runcommands/config.py
@@ -9,7 +9,7 @@ from subprocess import check_output, CalledProcessError, DEVNULL
 
 from .command import command
 from .const import DEFAULT_COMMANDS_MODULE
-from .exc import ConfigError, ConfigKeyError
+from .exc import ConfigError, ConfigKeyError, ConfigTypeError, ConfigValueError
 from .util import abort, abs_path, load_object
 
 try:
@@ -53,6 +53,8 @@ class RawConfig(OrderedDict):
             raise ConfigKeyError(name) from None
         if not isinstance(value, RawConfig):
             value = self._interpolate(value)
+        if isinstance(value, JSONValue):
+            value = value.load()
         return value
 
     def __setitem__(self, name, value):
@@ -131,29 +133,28 @@ class RawConfig(OrderedDict):
         return self if self._parent is None else self._parent._root
 
     def _interpolate(self, obj):
-        interpolated = []
-        value = self._do_interpolation(obj, interpolated)
-        while interpolated:
-            interpolated = []
-            value = self._do_interpolation(value, interpolated)
-        return value
-
-    def _do_interpolation(self, obj, interpolated):
         if isinstance(obj, str):
-            try:
-                new_value = obj.format(**self._root)
-            except KeyError as exc:
-                context = 'while interpolating into "{obj}"'.format(obj=obj)
-                raise ConfigKeyError(exc.args[0], context) from None
-            if new_value != obj:
-                obj = new_value
-                interpolated.append(obj)
+            root = self._root
+            obj, changed = self._inject(obj, root)
+            while changed:
+                obj, changed = self._inject(obj, root)
         elif isinstance(obj, Mapping):
             for key in obj:
-                obj[key] = self._do_interpolation(obj[key], interpolated)
+                obj[key] = self._interpolate(obj[key])
         elif isinstance(obj, Sequence):
-            obj = obj.__class__(self._do_interpolation(thing, interpolated) for thing in obj)
+            obj = obj.__class__(self._interpolate(thing) for thing in obj)
         return obj
+
+    def _inject(self, value, root=None):
+        root = self._root if root is None else root
+        try:
+            new_value = value.format(**root)
+        except KeyError as exc:
+            context = 'while interpolating into "{value}"'.format(value=value)
+            raise ConfigKeyError(exc.args[0], context) from None
+        if isinstance(value, JSONValue) and not isinstance(new_value, JSONValue):
+            new_value = JSONValue(new_value)
+        return new_value, new_value != value
 
     def _get_dotted(self, name, default=NO_DEFAULT):
         obj = self
@@ -324,24 +325,13 @@ class Config(RawConfig):
         return parser
 
     @classmethod
-    def _decode_value(cls, name, value, tolerant=False):
-        try:
-            value = json.loads(value)
-        except ValueError:
-            if tolerant:
-                return value
-            msg = 'Could not read {name} from config (not valid JSON): {value}'
-            raise ConfigError(msg.format_map(locals()))
-        return value
-
-    @classmethod
     def _get_envs(cls, file_name):
         parser = cls._make_config_parser(file_name)
         sections = set(parser.sections())
         for name in parser:
             extends = parser.get(name, 'extends', fallback=None)
             if extends:
-                extends = cls._decode_value('extends', extends)
+                extends = JSONValue(extends, name='extends').load()
                 sections.update(cls._get_envs(extends))
         return sorted(sections)
 
@@ -359,12 +349,11 @@ class Config(RawConfig):
 
         extends = section.get('extends')
         if extends:
-            extends = self._decode_value('extends', extends)
+            extends = JSONValue(extends, name='extends').load()
             self._read_from_file(extends, env)
 
         for name, value in section.items():
-            value = self._decode_value(name, value)
-            self._set_dotted(name, value)
+            self._set_dotted(name, JSONValue(value, name=name))
 
         if env and '__ENV_SECTION_FOUND_IN_FILE__' not in self:
             raise ConfigError('Env/section not found while reading config: {env}'.format(env=env))
@@ -386,6 +375,53 @@ class Config(RawConfig):
 class ConfigParser(RawConfigParser):
 
     optionxform = lambda self, name: name
+
+
+class JSONValue(str):
+
+    def __new__(cls, *args, name=None):
+        instance = super().__new__(cls, *args)
+        instance.name = name
+        return instance
+
+    @classmethod
+    def from_object(cls, obj, name=None):
+        value = json.dumps(obj)
+        return cls(value, name=name)
+
+    @classmethod
+    def loads(cls, value, name=None, tolerant=False):
+        """JSON str -> object"""
+        try:
+            obj = json.loads(value)
+        except TypeError as exc:
+            if tolerant:
+                args = exc.args + (name,)
+                raise ConfigTypeError(*args) from None
+            obj = value
+        except ValueError as exc:
+            if not tolerant:
+                args = exc.args + (name,)
+                raise ConfigValueError(*args) from None
+            obj = value
+        return obj
+
+    @classmethod
+    def dumps(cls, obj, name=None):
+        """object -> JSON str"""
+        try:
+            value = json.dumps(obj)
+        except TypeError as exc:
+            args = exc.args + (name,)
+            raise ConfigTypeError(*args)
+        except ValueError as exc:
+            args = exc.args + (name,)
+            raise ConfigValueError(*args)
+        return value
+
+    def load(self, tolerant=False):
+        """JSON str (self) -> object"""
+        return self.loads(self, self.name, tolerant)
 
 
 def version_getter(config):

runcommands/tests/commands.cfg
@@ -2,6 +2,9 @@
 version = "X.Y.Z"
 a = 1
 b = 2
-c = "{b}"
+c = {b}
 d.e = "{c}"
 x.y.z = "xyz"
+list.a = [1, 2, 3]
+list.b = {list.a}
+list.c = "{list.b}"

runcommands/tests/test_config.py
@@ -148,6 +148,18 @@ class TestRunConfig(TestCase):
 
 class TestConfig(TestCase):
 
+    def test_read_file(self):
+        config = Config()
+        self.assertEqual(config.version, 'X.Y.Z')
+        self.assertEqual(config.a, 1)
+        self.assertEqual(config.b, 2)
+        self.assertEqual(config.c, 2)
+        self.assertEqual(config.d.e, '2')
+        self.assertEqual(config._get_dotted('x.y.z'), 'xyz')
+        self.assertEqual(config.list.a, [1, 2, 3])
+        self.assertEqual(config.list.b, [1, 2, 3])
+        self.assertEqual(config.list.c, '[1, 2, 3]')
+
     def test_simple_interpolation(self):
         version = 'X.Y.Z'
         config = Config(run=RunConfig())
@@ -167,9 +179,3 @@ class TestConfig(TestCase):
         self.assertEqual(config.other, version)
         self.assertEqual(config.x.y, version)
         self.assertEqual(config.a.b.c, version)
-
-    def test_read_file(self):
-        config = Config()
-        self.assertEqual(config.version, 'X.Y.Z')
-        self.assertEqual(config.c, '2')
-        self.assertEqual(config.d.e, '2')

runcommands/command.py
@@ -630,7 +630,7 @@ class DictAddAction(argparse.Action):
                 .format_map(locals()))
 
         if value:
-            value = Config._decode_value(name, value, tolerant=True)
+            value = JSONValue(value, name=name).load(tolerant=True)
         else:
             value = None
 
@@ -644,11 +644,12 @@ class ListAppendAction(argparse.Action):
             setattr(namespace, self.dest, [])
         items = getattr(namespace, self.dest)
         if value:
-            value = Config._decode_value(len(items), value, tolerant=True)
+            name = str(len(items))
+            value = JSONValue(value, name=name).load(tolerant=True)
         else:
             value = None
         items.append(value)
 
 
 # Avoid circular import
-from .config import Config, RunConfig  # noqa: E402
+from .config import Config, JSONValue, RunConfig  # noqa: E402

runcommands/exc.py
@@ -20,13 +20,34 @@ class ConfigError(RunCommandsError):
 
 class ConfigKeyError(ConfigError, KeyError):
 
-    def __init__(self, key, context=None):
-        self.key = key
+    def __init__(self, *args, context=None):
         self.context = context
-        super().__init__(key, context)
-        KeyError.__init__(self, key)
+        super().__init__(*(args + (context,)))
+        KeyError.__init__(self, *args)
 
     def __str__(self):
-        string = '{self.__class__.__name__}{context}: {self.key}'
-        context = ' ({self.context})'.format(self=self) if self.context else ''
+        args = ', '.join(str(arg) for arg in self.args)
+        context = ' ({self.context})'.format(self=self) if self.context is not None else ''
+        string = '{self.__class__.__name__}{context}: {args}'
+        return string.format_map(locals())
+
+
+class ConfigTypeError(ConfigError, TypeError):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        TypeError.__init__(self, *args)
+
+
+class ConfigValueError(ConfigError, ValueError):
+
+    def __init__(self, *args, name=None):
+        self.name = name
+        super().__init__(*(args + (name,)))
+        ValueError.__init__(self, *args)
+
+    def __str__(self):
+        args = ', '.join(str(arg) for arg in self.args)
+        context = ' ({self.name})'.format(self=self) if self.name is not None else ''
+        string = '{self.__class__.__name__}{context}: {args}'
         return string.format_map(locals())

commit 790bb56258316a4771d4e99a9f558d1cb10985f9

Revamp config interpolation
`${key}` is now used instead of `{key}` and config values are parsed for
interpolation markers instead of using `str.format()`. This allows for
dict values (broken after 3d43275) and fixes some more complex
interpolation scenarios.

runcommands/config.py
@@ -135,9 +135,12 @@ class RawConfig(OrderedDict):
     def _interpolate(self, obj):
         if isinstance(obj, str):
             root = self._root
-            obj, changed = self._inject(obj, root)
+            obj_type = type(obj)
+            changed = True
             while changed:
                 obj, changed = self._inject(obj, root)
+            if not isinstance(obj, obj_type):
+                obj = obj_type(obj)
         elif isinstance(obj, Mapping):
             for key in obj:
                 obj[key] = self._interpolate(obj[key])
@@ -147,14 +150,68 @@ class RawConfig(OrderedDict):
 
     def _inject(self, value, root=None):
         root = self._root if root is None else root
-        try:
-            new_value = value.format(**root)
-        except KeyError as exc:
-            context = 'while interpolating into "{value}"'.format(value=value)
-            raise ConfigKeyError(exc.args[0], context) from None
-        if isinstance(value, JSONValue) and not isinstance(new_value, JSONValue):
-            new_value = JSONValue(new_value)
-        return new_value, new_value != value
+
+        begin, end = '${', '}'
+
+        if begin not in value:
+            return value, False
+
+        new_value = value
+        begin_pos, end_pos = 0, None
+        len_begin, len_end = len(begin), len(end)
+        len_value = len(new_value)
+        f = locals()
+
+        while begin_pos < len_value:
+            # Find next ${.
+            begin_pos = new_value.find(begin, begin_pos)
+
+            if begin_pos == -1:
+                break
+
+            # Save everything before ${.
+            before = new_value[:begin_pos]
+
+            # Find } after ${.
+            begin_pos += len_begin
+            end_pos = new_value.find(end, begin_pos)
+            if end_pos == -1:
+                message = 'Unmatched {begin}...{end} in {value}'.format_map(f)
+                raise ConfigValueError(message) from None
+
+            # Get name between ${ and }, ignoring leading and trailing
+            # whitespace.
+            name = new_value[begin_pos:end_pos]
+            name = name.strip()
+
+            if not name:
+                message = 'Empty name in {value}'.format_map(f)
+                raise ConfigValueError(message) from None
+
+            # Save everything after }.
+            after_pos = end_pos + len_end
+            after = new_value[after_pos:]
+
+            # Retrieve string value for named setting (the "injection
+            # value").
+            try:
+                injection_value = root._get_dotted(name)
+            except KeyError:
+                context = 'while interpolating into {value}'.format_map(f)
+                raise ConfigKeyError(name, context=context) from None
+            else:
+                if not isinstance(injection_value, str):
+                    injection_value = JSONValue.dumps(injection_value, name=name)
+
+            # Combine before, inject value, and after to get the new
+            # value.
+            new_value = ''.join((before, injection_value, after))
+
+            # Continue after injected value.
+            begin_pos = len(before) + len(injection_value)
+            len_value = len(new_value)
+
+        return new_value, (new_value != value)
 
     def _get_dotted(self, name, default=NO_DEFAULT):
         obj = self

runcommands/tests/commands.cfg
@@ -2,9 +2,15 @@
 version = "X.Y.Z"
 a = 1
 b = 2
-c = {b}
-d.e = "{c}"
+c = ${b}
+d.e = "${c}"
 x.y.z = "xyz"
 list.a = [1, 2, 3]
-list.b = {list.a}
-list.c = "{list.b}"
+list.b = ${list.a}
+list.c = "${list.b}"
+
+my.list = ["item"]
+my.other_list = ${my.list}
+my.dict = {"key": ${my.list}}
+
+dollar_sign = "$"

runcommands/tests/test_config.py
@@ -46,7 +46,7 @@ class TestRawConfig(TestCase):
     def test_get_via_get_method(self):
         config = RawConfig()
         config.x = 'x'
-        config.y = '{x}'
+        config.y = '${x}'
         self.assertEqual(config.get('y'), 'x')
 
     def test_item_can_be_retrieved_via_attribute_access(self):
@@ -99,7 +99,7 @@ class TestRawConfig(TestCase):
     def test_remove_via_pop_method(self):
         config = RawConfig()
         config.x = 'x'
-        config.y = '{x}'
+        config.y = '${x}'
         self.assertIn('y', config)
         y = config.pop('y')
         self.assertNotIn('y', config)
@@ -117,14 +117,14 @@ class TestRawConfig(TestCase):
         config = RawConfig()
         config.a = 1
         config.b = 2
-        config.c = '{b}'
+        config.c = '${b}'
         self.assertEqual(list(config.items()), [('a', 1), ('b', 2), ('c', '2')])
 
     def test_values(self):
         config = RawConfig()
         config.a = 1
         config.b = 2
-        config.c = '{b}'
+        config.c = '${b}'
         self.assertEqual(list(config.values()), [1, 2, '2'])
 
 
@@ -159,22 +159,26 @@ class TestConfig(TestCase):
         self.assertEqual(config.list.a, [1, 2, 3])
         self.assertEqual(config.list.b, [1, 2, 3])
         self.assertEqual(config.list.c, '[1, 2, 3]')
+        self.assertEqual(config.my.list, ['item'])
+        self.assertEqual(config.my.other_list, ['item'])
+        self.assertEqual(config.my.dict, {'key': ['item']})
+        self.assertEqual(config.dollar_sign, '$')
 
     def test_simple_interpolation(self):
         version = 'X.Y.Z'
         config = Config(run=RunConfig())
         config.version = version
-        config.other = '{version}'
+        config.other = '${version}'
         self.assertEqual(config.version, version)
         self.assertEqual(config.other, version)
 
-    def test_complex_interpolation(self):
+    def test_moderately_complex_interpolation(self):
         version = 'X.Y.Z'
         config = Config(run=RunConfig())
         config.version = version
-        config.other = '{version}'
-        config._set_dotted('x.y', '{other}')
-        config._set_dotted('a.b.c', '{x.y}')
+        config.other = '${version}'
+        config._set_dotted('x.y', '${other}')
+        config._set_dotted('a.b.c', '${x.y}')
         self.assertEqual(config.version, version)
         self.assertEqual(config.other, version)
         self.assertEqual(config.x.y, version)