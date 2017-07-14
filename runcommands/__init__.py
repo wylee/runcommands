a955b86e3fee625afc406f212715cee9b935e248
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-07-14 13:07:46 -0700

    Resume development at 1.0a26

--- runcommands/__init__.py
+++ runcommands/__init__.py
@@ -6,7 +6,7 @@ from .const import DEFAULT_ENV
 
 
 __all__ = ['__version__', 'DEFAULT_ENV', 'bool_or', 'command', 'configure']
-__version__ = '1.0a25'
+__version__ = '1.0a26.dev0'
 
 
 def configure(commands_module=None, config_file=None, default_env=None, env=None, echo=None,
