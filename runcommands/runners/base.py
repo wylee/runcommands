21d5d2a6bbeaad698f39df3f44a9972eb68cb6d8
Wyatt Baldwin <self@wyattbaldwin.com>
2017-10-28 15:32:24 -0700

    Add Runner.unbuffer_stdin()
    This makes stdin char-buffered instead of line-buffered. The code for
    this was copied from Fabric (although Fabric implements this as a
    context manager whereas this version unbuffers stdin and returns a
    function that resets it).

--- runcommands/runners/base.py
+++ runcommands/runners/base.py
@@ -3,9 +3,11 @@ import locale
 import os
 import pty
 import sys
+import termios
+import tty
 
 from .result import Result
-from ..util import printer
+from ..util import isatty, printer
 
 
 class Runner(metaclass=abc.ABCMeta):
@@ -13,6 +15,33 @@ class Runner(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def run(self, *args, **kwargs) -> Result:
         raise NotImplementedError('The run method must be implemented in subclasses')
+
+    def unbuffer_stdin(self, stdin):
+        """Make ``stdin`` char buffered instead of line buffered.
+
+        Return a function that resets ``stdin`` to its previous buffer
+        settings. This is intended to be used with try/finally like so::
+
+        reset_stdin = self.unbuffer_stdin(sys.stdin)
+        try:
+            # do stuff with unbuffered stdin
+        finally:
+            reset_stdin()
+
+        .. note:: This was taken from Fabric's ``char_buffered`` context
+                  manager and tweaked slightly to work with try/finally.
+
+        """
+        if not isatty(stdin):
+            return lambda: None
+
+        original_term_settings = termios.tcgetattr(stdin)
+        tty.setcbreak(stdin)
+
+        def reset():
+            termios.tcsetattr(stdin, termios.TCSADRAIN, original_term_settings)
+
+        return reset
 
     def get_encoding(self):
         return locale.getpreferredencoding(do_setlocale=False)
