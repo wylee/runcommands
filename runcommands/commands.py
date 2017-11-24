73cd28f970b25d20ffb5eac8698ed6e6d1c08a0e
Wyatt Baldwin <self@wyattbaldwin.com>
2017-11-23 20:49:39 -0800

    Add copy_file command
    It's basically a wrapper around `shutil.copy()` that adds some extra
    functionality:
    
    - The source and destination names will have `config` values injected
      using `.format_map(config)`
    - The source file can be rendered as a template using `str.format_map()`
      or `string.Template()`
    
    I've found this useful often enough that it makes sense to include it
    here rather than rewriting it in different projects.

--- runcommands/commands.py
+++ runcommands/commands.py
@@ -1,4 +1,5 @@
 from .config import show_config
 from .runners.commands import local, remote
+from .util.commands import copy_file
 
-__all__ = ['local', 'remote', 'show_config']
+__all__ = ['copy_file', 'local', 'remote', 'show_config']
