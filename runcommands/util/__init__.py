d617cb680217cc08e172ace4da4059c502004e0c
Wyatt Baldwin <self@wyattbaldwin.com>
2019-11-09 13:57:48 -0800

    Remove magical collect_commands() utility
    This was a lot to maintain for no real benefit. It was used in just one
    place internally and wasn't documented publicly.
    
    Drawbacks:
    
    - Too magical (frame hackery)
    - Hard to understand
    - Nonexplicit
    - Additional maintenance effort for not much benefit (doesn't save much
      effort over just doing manual imports)

--- runcommands/util/__init__.py
+++ runcommands/util/__init__.py
@@ -1,7 +1,4 @@
-import importlib
 import inspect
-import os
-import pathlib
 from collections import OrderedDict
 
 from .decorators import cached_property
@@ -14,7 +11,7 @@ from .string import camel_to_underscore
 
 
 __all__ = [
-    'collect_commands', 'get_commands_in_namespace',
+    'get_commands_in_namespace',
     'cached_property',
     'Color', 'StreamOptions',
     'abort', 'flatten_args', 'format_if', 'isatty', 'load_object', 'merge_dicts',
@@ -23,75 +20,6 @@ __all__ = [
     'confirm', 'prompt',
     'camel_to_underscore',
 ]
-
-
-def collect_commands(package_name=None, in_place=False, level=1):
-    """Collect commands from package and its subpackages.
-
-    This replaces the tedium of adding and maintaining a bunch of
-    imports like ``from .xyz import x, y, z`` in modules that are used
-    to collect all of the commands in a package.
-
-    Args:
-        package_name (str): Package to collect from. If not passed, the
-            package containing the module of the call site will be used.
-        in_place (bool): If set, the call site's globals will be updated
-            in place (using some frame magic).
-        level (int): If not called from the global scope, set this
-            appropriately to account for the call stack.
-
-    Returns:
-        OrderedDict: The commands found in the package, ordered by name.
-
-    Example usage::
-
-        # mypackage.commands
-        __all__ = list(collect_commands(in_place=True))
-
-    Less magical usage::
-
-        # mypackage.commands
-        commands = collect_commands()
-        globals().update(commands)
-        __all__ = list(commands)
-
-    .. note:: If ``package_name`` is passed and refers to a namespace
-        package, all corresponding namespace package directories will be
-        searched for commands.
-
-    """
-    commands = {}
-
-    frame = inspect.stack()[level][0]
-    f_globals = frame.f_globals
-
-    if package_name is None:
-        # Collect from package containing module of call site
-        package_name = f_globals['__name__'].rsplit('.', 1)[0]
-        package_paths = [os.path.dirname(f_globals['__file__'])]
-    else:
-        # Collect from named package
-        package = importlib.import_module(package_name)
-        package_name = package.__name__
-        package_paths = package.__path__
-
-    for package_path in package_paths:
-        package_path = pathlib.Path(package_path)
-        for file in package_path.rglob('*.py'):
-            rel_path = str(file.relative_to(package_path))
-            rel_path = rel_path[:-3]
-            module_name = rel_path.replace(os.sep, '.')
-            module_name = '.'.join((package_name, module_name))
-            module = importlib.import_module(module_name)
-            module_commands = get_commands_in_namespace(module)
-            commands.update(module_commands)
-
-    commands = OrderedDict((name, commands[name]) for name in sorted(commands))
-
-    if in_place:
-        f_globals.update(commands)
-
-    return commands
 
 
 def get_commands_in_namespace(namespace=None, level=1):
