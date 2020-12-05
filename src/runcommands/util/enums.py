4ee5e65c1cd9adabe1738e8a5fed1ea7e5e13382
Wyatt Baldwin <self@wyattbaldwin.com>
2020-12-05 12:45:09 -0800

    Use blessings to specify colors for util.Printer

--- src/runcommands/util/enums.py
+++ src/runcommands/util/enums.py
@@ -1,19 +1,24 @@
 import enum
 import subprocess
 
+from blessings import Terminal
+
+
+TERM = Terminal()
+
 
 class Color(enum.Enum):
 
     none = ""
-    reset = "\033[0m"
-    black = "\033[90m"
-    red = "\033[91m"
-    green = "\033[92m"
-    yellow = "\033[93m"
-    blue = "\033[94m"
-    magenta = "\033[95m"
-    cyan = "\033[96m"
-    white = "\033[97m"
+    reset = TERM.normal
+    black = TERM.black
+    red = TERM.red
+    green = TERM.green
+    yellow = TERM.yellow
+    blue = TERM.blue
+    magenta = TERM.magenta
+    cyan = TERM.cyan
+    white = TERM.white
 
     def __str__(self):
         return self.value
