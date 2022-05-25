67efdefba3c3fb035a64532bbd28902bec1ae6f7
Wyatt Baldwin <self@wyattbaldwin.com>
2022-05-24 20:56:25 -0700

    Make some Printer methods print to stderr by default
    Amends f643217b39b0

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,8 +1,13 @@
+from contextlib import redirect_stderr, redirect_stdout
 from doctest import DocTestSuite
+from io import StringIO
+from unittest import TestCase
 
 import runcommands.util.misc
 import runcommands.util.path
 import runcommands.util.string
+
+from runcommands.util.printer import printer
 
 
 def load_tests(loader, tests, ignore):
@@ -10,3 +15,29 @@ def load_tests(loader, tests, ignore):
     tests.addTests(DocTestSuite(runcommands.util.path))
     tests.addTests(DocTestSuite(runcommands.util.string))
     return tests
+
+
+class TestPrinter(TestCase):
+    def test_prints_to_stdout(self):
+        stdout = StringIO()
+        stderr = StringIO()
+        for attr in ("print", "header", "info", "success", "echo"):
+            with self.subTest(printer=attr):
+                with redirect_stdout(stdout):
+                    with redirect_stderr(stderr):
+                        attr = getattr(printer, attr)
+                        attr("stdout")
+            self.assertIn("stdout", stdout.getvalue())
+            self.assertEqual(stderr.getvalue(), "")
+
+    def test_prints_to_stderr(self):
+        stdout = StringIO()
+        stderr = StringIO()
+        for attr in ("warning", "error", "danger", "debug"):
+            with self.subTest(printer=attr):
+                with redirect_stdout(stdout):
+                    with redirect_stderr(stderr):
+                        attr = getattr(printer, attr)
+                        attr("stderr")
+            self.assertEqual(stdout.getvalue(), "")
+            self.assertIn("stderr", stderr.getvalue())
